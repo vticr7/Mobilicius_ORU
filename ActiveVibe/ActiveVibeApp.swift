f04f3d1882261e13916dba2ac72e5d64f97a8562
vticr7 <vti9227@gmail.com>
2024-01-09 00:50:26 +0530

    Initial Commit

---
+++ ActiveVibe/ActiveVibeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActiveVibeApp.swift
+//  ActiveVibe
+//
+//  Created by Vaibhav  Tiwary on 09/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ActiveVibeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
